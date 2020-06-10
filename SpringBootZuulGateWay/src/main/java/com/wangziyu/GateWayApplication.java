2c6130b7877137d5552d433793a9f3143c7edcc7
wangziyu <2958076014@qq.com>
2020-06-10 19:02:45 +0800

    update by wangziyu 20200610  10:19

--- SpringBootZuulGateWay/src/main/java/com/wangziyu/GateWayApplication.java
+++ SpringBootZuulGateWay/src/main/java/com/wangziyu/GateWayApplication.java
@@ -1,10 +1,13 @@
 package com.wangziyu;
 
 
+import com.wangziyu.gateway.WzyGateWay1;
+import com.wangziyu.gateway.WzyGateWay2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -14,7 +17,17 @@ public class GateWayApplication {
 
 
     public static void main(String args[]){
+
         SpringApplication.run(GateWayApplication.class);
+
     }
 
+    @Bean
+    public WzyGateWay1 getWzyGateway1(){
+        return new WzyGateWay1();
+    }
+    @Bean
+    public WzyGateWay2 getWzyGateway2(){
+        return new WzyGateWay2();
+    }
 }
