a3ad77d4da6d9145d4c5cceb844e3e63470ee62e
wangziyu <2958076014@qq.com>
2021-01-21 16:17:49 +0800

    add by wangziyu 20210121 17:03

---
+++ JavaBaseStudy/src/main/java/com/wangziyu/collectionstudy/model/Employee.java
@@ -0,0 +1,17 @@
+package com.wangziyu.collectionstudy.model;
+
+public class Employee {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employee(String name) {
+        this.name = name;
+    }
+}
